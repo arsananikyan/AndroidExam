f26f54cc8d2dbf2169c09d5f6eabceef9a8b5753
arsananikyan <arsananikyan@gmail.com>
2016-03-27 22:44:16 +0400

    Task is Done

---
+++ app/src/main/java/com/picsarttraining/androidexam/AboutActivity.java
@@ -0,0 +1,55 @@
+package com.picsarttraining.androidexam;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+public class AboutActivity extends AppCompatActivity {
+    private GoogleApiClient mClient;
+    private Uri mUrl;
+    private String mTitle;
+    private String mDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        mClient = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        mUrl = Uri.parse("http://www.android_exam.com/about");
+        mTitle = "About Screen";
+        mDescription = "Screen about us";
+    }
+
+    public Action getAction() {
+        Thing object = new Thing.Builder()
+                .setName(mTitle)
+                .setDescription(mDescription)
+                .setUrl(mUrl)
+                .build();
+
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mClient.connect();
+        AppIndex.AppIndexApi.start(mClient, getAction());
+    }
+
+    @Override
+    public void onStop() {
+        AppIndex.AppIndexApi.end(mClient, getAction());
+        mClient.disconnect();
+        super.onStop();
+    }
+}
--- app/src/main/java/com/picsarttraining/androidexam/MainActivity.java
+++ app/src/main/java/com/picsarttraining/androidexam/MainActivity.java
@@ -1,12 +1,18 @@
 package com.picsarttraining.androidexam;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Path;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -33,6 +39,7 @@ import java.util.TreeSet;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int SHARE_REQUEST = 10051;
     private GridView gridView;
     private DrawingView drawingView;
     private ArrayList<Uri> imageUris;
@@ -81,14 +88,63 @@ public class MainActivity extends AppCompatActivity {
             case R.id.share_button:
                 shareImage();
                 return true;
+            case R.id.about_button:
+                goToAboutScreen();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //I can't check resultCode==OK, because facebook always send RESUTL_CANCELED. Maybe because shareing with startActivity is bad practice
+        if (requestCode == SHARE_REQUEST) {
+            sendNotification();
+        }
+    }
+
+    private void sendNotification() {
+        int notificationId = 10;
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.side_nav_bar)
+                        .setContentTitle("My notification")
+                        .setContentText("Share is Complete");
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotificationManager.notify(notificationId, mBuilder.build());
+    }
+
+    private void goToAboutScreen() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("android_exam://about"));
+        startActivity(intent);
+    }
+
+
     private void shareImage() {
         try {
-            Bitmap finalBitmap = drawingView.getBitmap();
+            Bitmap myBitmap = drawingView.getBitmap();
+            Bitmap finalBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), myBitmap.getConfig());
+            Canvas canvas = new Canvas(finalBitmap);
+            canvas.drawColor(Color.WHITE);
+            canvas.drawBitmap(myBitmap, 0, 0, null);
+
             String path = Environment.getExternalStorageDirectory().toString();
             OutputStream fOut = null;
             File file = new File(path, "scrstasdasd.jpg");
@@ -104,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
             Intent share = new Intent(Intent.ACTION_SEND);
             share.setType("image/jpeg");
             share.putExtra(Intent.EXTRA_STREAM, finalPath);
-            startActivity(Intent.createChooser(share, "Share Image"));
+            startActivityForResult(Intent.createChooser(share, "Share Image"), SHARE_REQUEST);
         } catch (IOException e) {
             e.printStackTrace();
         }
